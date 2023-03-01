52b6d414e184be880db3f9d724264f8552a77767
williamjlago <126090881+williamjlago@users.noreply.github.com>
2023-03-01 10:32:25 -0500

    Add files via upload
    Added BinarySearchTree.java skeleton

---
+++ BinarySearchTree.java
@@ -0,0 +1,75 @@
+package lab6;
+import java.util.*;
+
+public class BinarySearchTree {
+	
+	public class Node {
+		int data;
+		Node subLeft;
+		Node subRight;
+	}
+	
+	boolean add(E item) {
+		
+	}
+	
+	E remove(E item) {
+		
+	}
+	
+	boolean find(E item) {
+		
+	}
+
+	E getParent(E item) {
+		
+	}
+	
+	ArrayList<E> getAllDescendant(E item) {
+		
+	}
+	
+	E getMax() {
+		
+	}
+	
+	int getHeight() {
+		return 0;
+	}
+	
+	int getLevel(E item) {
+		
+	}
+	
+	void inOrder() {
+		
+	}
+	
+	void preOrder() {
+		
+	}
+	
+	void postOrder() {
+		
+	}
+	
+	ArrayList<E> bfs() {
+		
+	}
+	
+	boolean isIdentical(Node<E> anotherTree) {
+		
+	}
+	
+	int numLeaves() {
+		return 0;
+	}
+	
+	int numInternal() {
+		return 0;
+	}
+	
+	void clear() {
+		
+	}
+}
